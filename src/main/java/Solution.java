64a17b2a0d846ff07f19eadd3b8231bfc6de68c3
alek <wait82985@gmail.com>
2023-06-21 13:39:25 +0600

    simplier algo. still TLE

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,36 +1,18 @@
-import java.util.Arrays;
-
 public class Solution {
     public long minCost(int[] nums, int[] cost) {
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        for (int x : nums) {
-            if (x < min) {
-                min = x;
+        long minResult = Long.MAX_VALUE;
+        long tempResult = 0;
+        for (int k = 0; k < nums.length; k++) {
+            int i = nums[k];
+            for (int j = 0; j < nums.length; j++) {
+                tempResult += Math.abs(nums[j] - i) * (long) (cost[j]);
+                if (tempResult > minResult) break;
             }
-            if (x > max) {
-                max = x;
-            }
-        }
-        if(min == max) return 0;
-        long result = Long.MAX_VALUE;
-        for (int i = min; i <= max; i++) {
-            result = Math.min(result, dfs(nums, cost, 0, min, max, i));
+            minResult = Math.min(minResult, tempResult);
+            tempResult = 0;
         }
 
-        return result;
-    }
-    private long dfs(int[] nums, int[] cost, int index, int min, int max, int i){
-        if(index == nums.length){
-            return 0;
-        }
-
-        long result = 0;
-
-        result +=  dfs(nums, cost, index + 1, min, max, i) +  Math.abs(nums[index] - i) * (long) cost[index];
-
-
-        return result;
+        return minResult;
     }
 
 }
